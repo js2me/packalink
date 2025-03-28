cdd39cd495d8935e0e5da92a73560cea7b086b55
Sergey Volkov <js2me@outlook.com>
2025-03-28 16:10:36 +0300

    fix: try to fix using run command

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export * from './run/index.js';
-export * from './define-config.js';
+import {
+  defineConfig,
+  type PackalinkConfig,
+  type PackalinkLink,
+} from './define-config.js';
+import { run as runFn } from './run/index.js';
+
+export { runFn as run };
+export { defineConfig };
+export type { PackalinkConfig, PackalinkLink };
